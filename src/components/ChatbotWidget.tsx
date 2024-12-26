d82b052570fee18524bacb68cea7e6478a1e47b7
Chris-June <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>
2024-12-26 17:19:03 -0500

    Update netlify.toml with CSP and CORS headers for chatbot script

--- src/components/ChatbotWidget.tsx
+++ src/components/ChatbotWidget.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const ChatbotWidget: React.FC = () => {
-  return (
-    <>
-      <div id="intellisync-chat"></div>
-      <script 
-        src="https://intellisync-server-fbc47e09b67b.herokuapp.com/chatbot-embed.js" 
-        data-intellisync-id="pNXqEuc4jTU3Y07ylF5I" 
-        defer 
-        type="module"
-      />
-    </>
-  );
+  useEffect(() => {
+    // Dynamically load the chatbot script
+    const script = document.createElement('script');
+    script.src = 'https://intellisync-server-fbc47e09b67b.herokuapp.com/chatbot-embed.js';
+    script.setAttribute('data-intellisync-id', 'pNXqEuc4jTU3Y07ylF5I');
+    script.type = 'module';
+    script.crossOrigin = 'anonymous';
+    script.defer = true;
+
+    // Ensure the script is added only once
+    if (!document.querySelector(`script[src="${script.src}"]`)) {
+      document.body.appendChild(script);
+    }
+
+    return () => {
+      // Optional: Remove the script if component unmounts
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
+  }, []);
+
+  return <div id="intellisync-chat"></div>;
 };
 
 export default ChatbotWidget;
