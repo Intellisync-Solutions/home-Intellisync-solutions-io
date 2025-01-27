f677dfce4a2c43e068c375a5671c45e442530098
Chris June <chris.june@intellisync.ca>
2025-01-26 19:18:29 -0500

    Update CSP default-src and frame-src directives for better framing support

--- vite.config.ts
+++ vite.config.ts
@@ -36,19 +36,37 @@ export default defineConfig({
       'X-Frame-Options': 'SAMEORIGIN',
       'X-Content-Type-Options': 'nosniff',
       'X-XSS-Protection': '1; mode=block',
-      'Content-Security-Policy': "default-src 'self' https://intellisync-server-fbc47e09b67b.herokuapp.com https://fonts.googleapis.com https://fonts.gstatic.com https://intellisyncsolutions.io https://intellisync-solutions-io.netlify.app; " +
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
-        "script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
-        "frame-src 'self' https://intellisyncsolutions.io https://intellisync-server-fbc47e09b67b.herokuapp.com https://intellisync-solutions-io.netlify.app; " +
-        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-        "img-src 'self' data: https:; " +
-        "font-src 'self' data: https://fonts.gstatic.com; " +
-        "connect-src 'self' https: wss: http: ws: " +
+      'Content-Security-Policy': "default-src 'self' " +
         "https://intellisync-server-fbc47e09b67b.herokuapp.com " +
         "https://fonts.googleapis.com " +
         "https://fonts.gstatic.com " +
         "https://intellisyncsolutions.io " +
-        "https://intellisync-solutions-io.netlify.app"
+        "https://www.googletagmanager.com " +
+        "https://www.google-analytics.com " +
+        "https://intellisync-solutions-io.netlify.app " +
+        "wss://intellisync-server-fbc47e09b67b.herokuapp.com " +
+        "ws://intellisync-server-fbc47e09b67b.herokuapp.com; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
+        "script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
+        "frame-src 'self' " +
+        "https://intellisyncsolutions.io " +
+        "https://intellisync-server-fbc47e09b67b.herokuapp.com " +
+        "https://intellisync-solutions-io.netlify.app " +
+        "https://www.googletagmanager.com " +
+        "https://www.google-analytics.com; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+        "img-src 'self' data: https: https://www.googletagmanager.com https://www.google-analytics.com; " +
+        "font-src 'self' data: https://fonts.gstatic.com; " +
+        "connect-src 'self' https: wss: ws: " +
+        "wss://intellisync-server-fbc47e09b67b.herokuapp.com " +
+        "ws://intellisync-server-fbc47e09b67b.herokuapp.com " +
+        "https://intellisync-server-fbc47e09b67b.herokuapp.com " +
+        "https://fonts.googleapis.com " +
+        "https://fonts.gstatic.com " +
+        "https://intellisyncsolutions.io " +
+        "https://intellisync-solutions-io.netlify.app " +
+        "https://www.google-analytics.com " +
+        "https://www.googletagmanager.com"
     },
   },
 })
