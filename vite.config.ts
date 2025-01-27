9b33a90bb0715ae286483560deeb89adeb3df143
Chris June <chris.june@intellisync.ca>
2025-01-26 19:11:42 -0500

    Add explicit script-src-elem directive to CSP

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,8 @@ export default defineConfig({
       'X-Content-Type-Options': 'nosniff',
       'X-XSS-Protection': '1; mode=block',
       'Content-Security-Policy': "default-src 'self' https://intellisync-server-fbc47e09b67b.herokuapp.com https://fonts.googleapis.com https://fonts.gstatic.com; " +
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
+        "script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
         "frame-src 'self' https://intellisyncsolutions.io https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
         "img-src 'self' data: https:; " +
