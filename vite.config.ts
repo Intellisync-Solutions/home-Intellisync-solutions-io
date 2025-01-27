5ee9cbf786d54f39525a921199d3f76aaed37ea7
Chris June <chris.june@intellisync.ca>
2025-01-26 19:15:09 -0500

    Expand CSP to allow more sources and websocket connections

--- vite.config.ts
+++ vite.config.ts
@@ -36,14 +36,19 @@ export default defineConfig({
       'X-Frame-Options': 'SAMEORIGIN',
       'X-Content-Type-Options': 'nosniff',
       'X-XSS-Protection': '1; mode=block',
-      'Content-Security-Policy': "default-src 'self' https://intellisync-server-fbc47e09b67b.herokuapp.com https://fonts.googleapis.com https://fonts.gstatic.com; " +
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
+      'Content-Security-Policy': "default-src 'self' https://intellisync-server-fbc47e09b67b.herokuapp.com https://fonts.googleapis.com https://fonts.gstatic.com https://intellisyncsolutions.io https://intellisync-solutions-io.netlify.app; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
         "script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
-        "frame-src 'self' https://intellisyncsolutions.io https://intellisync-server-fbc47e09b67b.herokuapp.com; " +
+        "frame-src 'self' https://intellisyncsolutions.io https://intellisync-server-fbc47e09b67b.herokuapp.com https://intellisync-solutions-io.netlify.app; " +
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
         "img-src 'self' data: https:; " +
         "font-src 'self' data: https://fonts.gstatic.com; " +
-        "connect-src 'self' https:"
+        "connect-src 'self' https: wss: http: ws: " +
+        "https://intellisync-server-fbc47e09b67b.herokuapp.com " +
+        "https://fonts.googleapis.com " +
+        "https://fonts.gstatic.com " +
+        "https://intellisyncsolutions.io " +
+        "https://intellisync-solutions-io.netlify.app"
     },
   },
 })
